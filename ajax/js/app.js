b91847fecf211f46b06222b1045def363e34cc4c
iswy <iisswy@gmail.com>
2016-10-18 11:43:12 -0600

    🌳.4

--- ajax/js/app.js
+++ ajax/js/app.js
@@ -5,6 +5,11 @@ var main = function ()
 {
     "use strict";
 
+    $.getJSON("escuela/alumnos.json", function ( alumno )
+    {
+        console.log( "Alumno" + alumno );
+    });
+
     console.log( "Hello Words" );
 };
 
