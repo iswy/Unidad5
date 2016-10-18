b3cc59e7e9678f24499f2917a83a4925e2c74921
iswy <iisswy@gmail.com>
2016-10-18 11:56:18 -0600

    🌳.6

--- ajax/js/app.js
+++ ajax/js/app.js
@@ -8,14 +8,26 @@ var main = function ()
     $.getJSON("escuela/alumnos.json", function ( alumno )
     {
         console.log( "Alumno" + alumno );
+
+        var $pAlumno = $("<p>");
+        $pAlumno.text( alumno.nombre + " - " + alumno.semestre )
+
+        $("main").append($pAlumno);
     });
 
-    var $abc01 = JSON.stringify( $.getJSON("escuela/alumnos.json") );
-    var $abc = $("<p class='p1'>"+$abc01+"</p>");
-
-    $("main").append($abc);
-
+    //var $abc01 = JSON.stringify( $.getJSON("escuela/alumnos.json") );
+    //var $abc = $("<p class='p1'>"+$abc01+"</p>");
+    
     console.log( "Hello Words" );
+
+    /*
+    Tarea:
+        * Modificar el JSON para contener un arreglo de alumnos
+        * Modificar este JS para agregar los alumnos a una lista no-ordenada
+        * Agregar un archivo CSS para presentación.
+        * extra: agregar animaciones
+     */
+
 };
 
 $( document ).ready( main );
