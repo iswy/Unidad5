63933fd5ca2063ba0cb976ecbe96d45d881571e5
iswy <iisswy@gmail.com>
2016-10-18 19:51:55 -0600

    📚..1

--- ajax/js/app.js
+++ ajax/js/app.js
@@ -1,23 +1,39 @@
 /**
  * Created by pi on 16.10.18.
  */
+
+var jsonx = function ( alumno )
+{
+    console.log( "Alumno" + alumno );
+
+    var $pAlumno = $("<p>");
+    $pAlumno.text( alumno.nombre + " -|- " + alumno.semestre )
+
+    var $pTe = $("<p>");
+
+    $("main").append($pAlumno);
+
+    var i=0;
+    var x="";
+
+    for( ; i<10; i++ )
+    {
+        x += $pTe.text( "hello "+i );
+    }
+    $("main").append(x);
+
+
+}
+
 var main = function ()
 {
     "use strict";
 
-    $.getJSON("escuela/alumnos.json", function ( alumno )
-    {
-        console.log( "Alumno" + alumno );
-
-        var $pAlumno = $("<p>");
-        $pAlumno.text( alumno.nombre + " - " + alumno.semestre )
-
-        $("main").append($pAlumno);
-    });
+    $.getJSON("escuela/alumnos.json", jsonx );
 
     //var $abc01 = JSON.stringify( $.getJSON("escuela/alumnos.json") );
     //var $abc = $("<p class='p1'>"+$abc01+"</p>");
-    
+
     console.log( "Hello Words" );
 
     /*
